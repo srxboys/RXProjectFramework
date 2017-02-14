8af6fbd61d59f5bbea4a7410a25d6b42000896aa
srxboys <srxboys@126.com>
2017-02-14 17:46:17 +0800

    add project framework
    添加 多个target的空框架

---
+++ RXProjectFramework/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RXProjectFramework
+//
+//  Created by srx on 2017/2/14.
+//  Copyright © 2017年 https://github.com/srxboys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RXProjectFramework/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RXProjectFramework
+//
+//  Created by srx on 2017/2/14.
+//  Copyright © 2017年 https://github.com/srxboys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
